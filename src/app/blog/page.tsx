d7aef65166d8deba2fe5032667c70d5f4e54e989
saladmaki <milk.choco.1147@gmail.com>
2025-07-24 20:31:00 +0900

    commit-7

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import "./article-list.css";
+
+export default function ArticleList() {
+  return (
+    <div className="article-list">
+      <div className="content">
+        <h1>Welcome to Saladmaki's site.</h1>
+        <ul id="articles" className="articles">
+          {/* <li>
+            <Link href="about:blank">
+              <h2>Lorem, ipsum dolor.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptates, architecto.
+              </p>
+            </Link>
+          </li> */}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <small>
+        Made with Next.js. <br />
         Created By Saladmaki. <br />
         &copy; 2025 Saladmaki. All rights reserved.
       </small>
