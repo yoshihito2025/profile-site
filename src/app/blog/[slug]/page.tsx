01ccfa0b9a3494f9c57b73ad9129ed7d1dc240d2
saladmaki <milk.choco.1147@gmail.com>
2025-07-28 21:14:07 +0900

    commit-12

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 export const dynamic = "force-static";
 
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 import matter from "gray-matter";
 import { remark } from "remark";
@@ -9,7 +9,9 @@ import html from "remark-html";
 import styles from "./blog.module.css";
 
 type Props = {
-  params: { slug: string };
+  params: {
+    slug: string;
+  };
 };
 
 export const generateMetadata = () => {
@@ -21,7 +23,7 @@ export const generateMetadata = () => {
 
 export async function generateStaticParams() {
   const dir = path.join(process.cwd(), "src/app/blog/articles");
-  const files = fs.readdirSync(dir);
+  const files = await fs.readdir(dir);
   return files.map((filename) => ({
     slug: filename.replace(/\.md$/, ""),
   }));
@@ -32,17 +34,23 @@ export default async function ArticlePage({
 }: {
   params: { slug: string };
 }) {
+  const { slug } = await Promise.resolve(params);
+
   const filePath = path.join(
     process.cwd(),
     "src/app/blog/articles",
-    `${params.slug}.md`
+    `${slug}.md`
   );
 
-  if (!fs.existsSync(filePath)) {
+  let fileContents: string;
+  try {
+    fileContents = await fs.readFile(filePath, "utf8");
+  } catch (err) {
+    console.error("ファイル読み込み失敗: ", err);
     notFound();
   }
 
-  const fileContents = fs.readFileSync(filePath, "utf8");
+  // const fileContents = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContents);
 
   const processedContent = await remark().use(html).process(content);
